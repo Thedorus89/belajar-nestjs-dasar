0322e1018f43c739f41b0dae22bed6bc27c4120a
Thedorus89 <theodoredanang@gmail.com>
2024-09-09 13:24:01 +0700

    add user

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Header, HttpCode, HttpRedirectResponse, Param, Post, Query, Redirect, Req, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('/api/users')
+export class UserController {
+    @Get('/sample-response')
+    @Header("Content-Type","application/json")
+    @HttpCode(200)
+    sampleResponse(): Record<string,string> {
+        return {
+            "data": "Hello JSON",
+        };
+    }
+
+    @Get('/redirect')
+    @Redirect()
+    redirect(): HttpRedirectResponse{
+        return {
+            url: '/api/users/sample-response',
+            statusCode: 301
+        };
+    }
+
+    @Get('/hello')
+    sayHello(
+        @Query('first_Name') firstName: string,
+        @Query('last_Name') lastName: string,
+    ): string {
+        return `Hello ${firstName} ${lastName}`;
+    }
+
+    @Get("/:id")
+    getById(@Param("id") id: string): string{
+        return `GET ${id}`;
+    }
+    
+    @Post()
+    post(): string{
+        return 'POST'
+    }
+
+    @Get('/sample')
+    get(): string {
+        return 'Hello Nest Js';
+    }
+}
